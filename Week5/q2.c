4256ca5bb362892fd10ff955a62c706eed14cf63
mandarchaudhari0 <115355550+mandarchaudhari0@users.noreply.github.com>
2024-11-06 09:33:33 +0000

    Add all changes

---
+++ Week5/q2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<mpi.h>
+#include<ctype.h>
+
+int main(int argc, char* argv[])
+{
+    int rank,size;
+    MPI_Init(&argc,&argv);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    MPI_Comm_rank(MPI_COMM_WORLD,&size);
+    int num=0;
+
+    if(rank==0)
+    {
+        printf("Enter Number :\n");
+        scanf("%d",&num);
+
+        for(int i=0;i<size;i++)
+        {
+            MPI_Send(&num,1,MPI_INT,i,1,MPI_COMM_WORLD);
+        }
+    }
+
+    if(rank!=0)
+    {
+        MPI_Recv(&num,1,MPI_INT,0,1,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+
+        printf("Process: %d recieved number %d",rank,num);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
